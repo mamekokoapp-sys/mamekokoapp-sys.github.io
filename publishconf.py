e634c4fce3b4464b1157095f25271e98ddfd63d6
mamekokoapp-sys <mamekoko.app@gmail.com>
2026-06-19 16:05:18 +0000

    actionの追加

--- publishconf.py
+++ publishconf.py
@@ -9,12 +9,18 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "no"
-RELATIVE_URLS = False
+
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
+
+SITEURL = "https://mamekokoapp-sys.github.io"
+RELATIVE_URLS = False
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Following items are often useful when publishing
 
